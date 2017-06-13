54617991880de5f7e40d336915d4351f3e1c7909
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-06-13 11:52:59 +0200

    updated version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="softfire-sdk",
-    version="0.1.5",
+    version="0.1.6",
     author="Lorenzo Tomasini",
     author_email="lorenzo.tomasini@gmail.com",
     description="The SoftFIRE SDK for Manager",
